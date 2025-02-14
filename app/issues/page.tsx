20e6d2c200c70381ee3260e0ed01b4362fa28d08
DenisParise <dd.parise@gmail.com>
2025-02-14 20:42:46 +0100

    set up radix ui and issues api

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,7 +1,12 @@
+import { Button } from "@radix-ui/themes";
 import React from "react";
 
 const IssuesPage = () => {
-    return <div>&lt;blink&gt;herro wordo&lt;/blink&gt;</div>;
+    return (
+        <div>
+            <Button>New Issue </Button>
+        </div>
+    );
 };
 
 export default IssuesPage;
