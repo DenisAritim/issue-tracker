a3573a653d0439f9ae76f74e0532aac9d7a512f2
DenisParise <dd.parise@gmail.com>
2025-02-15 16:30:51 +0100

    feat: add spinner and button disabled on submit in new issue form

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -26,6 +26,7 @@ const NewIssuePage = () => {
         resolver: zodResolver(createIssueSchema),
     });
     const [error, setError] = useState("");
+    const [isSubmitting, setSubmitting] = useState(false);
 
     return (
         <div className="max-w-xl">
@@ -41,9 +42,11 @@ const NewIssuePage = () => {
                 className="space-y-5 flex-col"
                 onSubmit={handleSubmit(async (data) => {
                     try {
+                        setSubmitting(true);
                         await axios.post("/api/issues", data);
                         router.push("/issues");
                     } catch {
+                        setSubmitting(false);
                         setError("An unexpected error occurred");
                     }
                 })}
@@ -58,7 +61,7 @@ const NewIssuePage = () => {
                     )}
                 />
                 <ErrorMessage>{errors.description?.message}</ErrorMessage>
-                <Button>Submit New Issue</Button>
+                <Button loading={isSubmitting}>Submit New Issue</Button>
             </form>
         </div>
     );
