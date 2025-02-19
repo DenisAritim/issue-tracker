8c4d3172be5fc570c6dbc599c0b06eb20cc9bffb
DenisParise <dd.parise@gmail.com>
2025-02-19 15:15:03 +0100

    feat: add edit page and api

---
+++ app/api/issues/[id]/route.ts
@@ -0,0 +1,31 @@
+import { issueSchema } from "@/app/validationSchemas";
+import { prisma } from "@/lib/prisma";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function PATCH(
+    request: NextRequest,
+    { params }: { params: Promise<{ id: string }> }
+) {
+    const body = await request.json();
+    const validation = issueSchema.safeParse(body);
+    if (!validation.success)
+        return NextResponse.json(validation.error.format(), { status: 400 });
+
+    const { id: idString } = await params;
+    const id = parseInt(idString);
+
+    if (isNaN(id)) return NextResponse.json({ error: "Invalid id" }, { status: 400 });
+
+    const issue = await prisma.issue.findUnique({
+        where: { id },
+    });
+
+    if (!issue) return NextResponse.json({ error: "Invalid issue" }, { status: 404 });
+
+    const updatedIssue = await prisma.issue.update({
+        where: { id },
+        data: { title: body.title, description: body.description },
+    });
+
+    return NextResponse.json(updatedIssue);
+}
