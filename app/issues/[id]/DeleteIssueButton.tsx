519b62fcef1eb6707c39920d048cfde05e8c30eb
DenisAritim <dd.parise@gmail.com>
2025-02-19 19:51:27 +0100

    feat: handle errors on issue delete

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -2,44 +2,68 @@
 import { AlertDialog, Button, Flex } from "@radix-ui/themes";
 import axios from "axios";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 import { RxTrash } from "react-icons/rx";
 
 const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
     const router = useRouter();
+    const [error, setError] = useState(false);
+
+    const deleteIssue = async () => {
+        try {
+            await axios.delete("/api/issues/" + issueId);
+            router.push("/issues");
+        } catch {
+            setError(true);
+        }
+    };
+
     return (
-        <AlertDialog.Root>
-            <AlertDialog.Trigger>
-                <Button color="red">
-                    <RxTrash />
-                    Delete Issue
-                </Button>
-            </AlertDialog.Trigger>
-            <AlertDialog.Content>
-                <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
-                <AlertDialog.Description>
-                    Are you sure to want to delete this issue? The action cannot be
-                    undone.
-                </AlertDialog.Description>
-                <Flex gap="3" justify="end" mt="4" mr="6">
-                    <AlertDialog.Cancel>
-                        <Button color="gray" variant="soft">
-                            Cancel
-                        </Button>
-                    </AlertDialog.Cancel>
-                    <AlertDialog.Action>
-                        <Button
-                            color="red"
-                            onClick={async () => {
-                                await axios.delete("/api/issues/" + issueId);
-                                router.push("/issues");
-                            }}
-                        >
-                            Delete Issue
-                        </Button>
-                    </AlertDialog.Action>
-                </Flex>
-            </AlertDialog.Content>
-        </AlertDialog.Root>
+        <>
+            <AlertDialog.Root>
+                <AlertDialog.Trigger>
+                    <Button color="red">
+                        <RxTrash />
+                        Delete Issue
+                    </Button>
+                </AlertDialog.Trigger>
+                <AlertDialog.Content>
+                    <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
+                    <AlertDialog.Description>
+                        Are you sure to want to delete this issue? The action cannot be
+                        undone.
+                    </AlertDialog.Description>
+                    <Flex gap="3" justify="end" mt="4" mr="6">
+                        <AlertDialog.Cancel>
+                            <Button color="gray" variant="soft">
+                                Cancel
+                            </Button>
+                        </AlertDialog.Cancel>
+                        <AlertDialog.Action>
+                            <Button color="red" onClick={deleteIssue}>
+                                Delete Issue
+                            </Button>
+                        </AlertDialog.Action>
+                    </Flex>
+                </AlertDialog.Content>
+            </AlertDialog.Root>
+            <AlertDialog.Root open={error}>
+                <AlertDialog.Content>
+                    <AlertDialog.Title>Error</AlertDialog.Title>
+                    <AlertDialog.Description>
+                        This issue could not be deleted
+                    </AlertDialog.Description>
+                    <Button
+                        color="gray"
+                        variant="soft"
+                        mt="4"
+                        onClick={() => setError(false)}
+                    >
+                        OK
+                    </Button>
+                </AlertDialog.Content>
+            </AlertDialog.Root>
+        </>
     );
 };
 
