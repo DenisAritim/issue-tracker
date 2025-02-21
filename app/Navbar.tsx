8c83b4ed45fa8281ca091dba4613ea5e4e1ae828
DenisAritim <dd.parise@gmail.com>
2025-02-21 17:20:23 +0100

    feat: login and logout links, new navbar layout

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,10 +1,13 @@
 "use client";
+import { Container, Flex, Spinner } from "@radix-ui/themes";
+import { useSession } from "next-auth/react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { MdBugReport } from "react-icons/md";
 
 const Navbar = () => {
     const currentPath = usePathname();
+    const { status, data: session } = useSession();
 
     const links = [
         { label: "Dashboard", href: "/" },
@@ -12,26 +15,42 @@ const Navbar = () => {
     ];
 
     return (
-        <nav className="flex space-x-5 px-5 border-b shadow-sm mb-5 h-14 items-center">
-            <Link href="/">
-                <MdBugReport size="2.5rem" />
-            </Link>
-            <ul className="flex gap-5 px-5">
-                {links.map((l) => (
-                    <li key={l.href}>
-                        <Link
-                            className={`${
-                                currentPath === l.href
-                                    ? "text-zinc-900"
-                                    : "text-zinc-600"
-                            } hover:text-zinc-800 transition-colors`}
-                            href={l.href}
-                        >
-                            {l.label}
+        <nav className="px-5 border-b shadow-sm mb-5 py-4">
+            <Container>
+                <Flex justify="between">
+                    <Flex align="center">
+                        <Link href="/">
+                            <MdBugReport size="2rem" />
                         </Link>
-                    </li>
-                ))}
-            </ul>
+                        <ul className="flex gap-5 px-5">
+                            {links.map((l) => (
+                                <li key={l.href}>
+                                    <Link
+                                        className={`${
+                                            currentPath === l.href
+                                                ? "text-zinc-900"
+                                                : "text-zinc-600"
+                                        } hover:text-zinc-800 transition-colors`}
+                                        href={l.href}
+                                    >
+                                        {l.label}
+                                    </Link>
+                                </li>
+                            ))}
+                        </ul>
+                    </Flex>
+                    <Flex align="center" gap="2">
+                        {session && <span>{session.user?.name}</span>}
+                        {status === "authenticated" && (
+                            <Link href="/api/auth/signout">Logout</Link>
+                        )}
+                        {status === "unauthenticated" && (
+                            <Link href="/api/auth/signin">Login</Link>
+                        )}
+                        {status === "loading" && <Spinner />}
+                    </Flex>
+                </Flex>
+            </Container>
         </nav>
     );
 };
--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./Navbar";
+import AuthProvider from "./auth/Provider";
 
 const inter = Inter({
     subsets: ["latin"],
@@ -24,12 +25,14 @@ export default function RootLayout({
     return (
         <html lang="en" className={inter.variable}>
             <body className="antialiased">
-                <Theme accentColor="green" grayColor="mauve" hasBackground={false}>
-                    <Navbar />
-                    <main className="p-5">
-                        <Container>{children}</Container>
-                    </main>
-                </Theme>
+                <AuthProvider>
+                    <Theme accentColor="green" grayColor="mauve" hasBackground={false}>
+                        <Navbar />
+                        <main className="p-5">
+                            <Container>{children}</Container>
+                        </main>
+                    </Theme>
+                </AuthProvider>
             </body>
         </html>
     );
