4d8db41c9a8d237b61a7e92ffa9cd6c749b01e1c
DenisAritim <dd.parise@gmail.com>
2025-02-24 22:36:50 +0100

    feat: implement pagination

--- app/components/Pagination.tsx
+++ app/components/Pagination.tsx
@@ -1,5 +1,7 @@
 "use client";
 import { Button, Flex, Text } from "@radix-ui/themes";
+import { useSearchParams } from "next/navigation";
+import { useRouter } from "next/navigation";
 import {
     RxChevronLeft,
     RxChevronRight,
@@ -14,24 +16,53 @@ interface Props {
 }
 
 const Pagination = ({ itemCount, pageSize, currentPage }: Props) => {
+    const router = useRouter();
+    const searchParams = useSearchParams();
+
     const pageCount = Math.ceil(itemCount / pageSize);
     if (pageCount <= 1) return null;
 
+    const changePage = (page: number) => {
+        const params = new URLSearchParams(searchParams);
+        params.set("page", page.toString());
+        router.push("?" + params);
+    };
+
     return (
         <Flex align="center" gap="2">
-            <Button size="1" variant="soft" disabled={currentPage === 1 && true}>
+            <Button
+                size="1"
+                variant="soft"
+                disabled={currentPage === 1 && true}
+                onClick={() => changePage(1)}
+            >
                 <RxDoubleArrowLeft />
             </Button>
-            <Button size="1" variant="soft" disabled={currentPage === 1 && true}>
+            <Button
+                size="1"
+                variant="soft"
+                disabled={currentPage === 1 && true}
+                onClick={() => changePage(currentPage - 1)}
+            >
                 <RxChevronLeft />
             </Button>
             <Text>
                 Page {currentPage} of {pageCount}
             </Text>
-            <Button size="1" variant="soft" disabled={currentPage === pageCount && true}>
+            <Button
+                size="1"
+                variant="soft"
+                disabled={currentPage === pageCount && true}
+                onClick={() => changePage(currentPage + 1)}
+            >
                 <RxChevronRight />
             </Button>
-            <Button size="1" variant="soft" disabled={currentPage === pageCount && true}>
+            <Button
+                size="1"
+                variant="soft"
+                disabled={currentPage === pageCount && true}
+                onClick={() => changePage(pageCount)}
+            >
                 <RxDoubleArrowRight />
             </Button>
         </Flex>
