c1d797a877147d600336ec6a636703b81c776afe
DenisAritim <dd.parise@gmail.com>
2025-02-24 16:43:39 +0100

    feat: issues sortable

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -2,23 +2,64 @@ import { prisma } from "@/lib/prisma";
 import { Table } from "@radix-ui/themes";
 import { IssueStatusBadge, Link } from "../components";
 import IssueToolbar from "./IssueToolbar";
-import { Status } from "@prisma/client";
+import { Issue, Status } from "@prisma/client";
+import NextLink from "next/link";
+import { RxArrowDown, RxArrowUp } from "react-icons/rx";
 
 interface Props {
-    searchParams: Promise<{ status: Status }>;
+    searchParams: Promise<{
+        status: Status;
+        orderBy: keyof Issue;
+        orderDirection: "asc" | "desc";
+    }>;
 }
 
 const IssuesPage = async ({ searchParams }: Props) => {
+    const status = (await searchParams).status;
+    const orderBy = (await searchParams).orderBy;
+    const orderDirection = (await searchParams).orderDirection;
+
+    const columns: { label: string; value: keyof Issue; className?: string }[] = [
+        {
+            label: "Issue",
+            value: "title",
+        },
+        {
+            label: "Status",
+            value: "status",
+            className: "hidden md:table-cell",
+        },
+        {
+            label: "Created",
+            value: "createdAt",
+            className: "hidden md:table-cell",
+        },
+        {
+            label: "Assigned to",
+            value: "assignedToUserId",
+            className: "hidden md:table-cell",
+        },
+    ];
+
     const validStatus = Object.values(Status);
+    const validStatusQuery = validStatus.includes(status) ? status : undefined;
 
-    const statusQuery = (await searchParams).status;
-
-    const status = validStatus.includes(statusQuery) ? statusQuery : undefined;
+    const validOrder = columns.map((column) => column.value);
+    const validOrderQuery = validOrder.includes(orderBy) ? orderBy : undefined;
 
     const issues = await prisma.issue.findMany({
-        where: { status },
+        where: { status: validStatusQuery },
+        orderBy: validOrderQuery ? { [orderBy]: orderDirection } : undefined,
         include: { assignedToUser: true },
     });
+
+    const buildQuery = (orderBy: string, orderDirection: "asc" | "desc") => {
+        const query = new URLSearchParams();
+        if (validStatusQuery) query.append("status", validStatusQuery);
+        query.append("orderBy", orderBy);
+        query.append("orderDirection", orderDirection);
+        return query.toString();
+    };
 
     return (
         <div>
@@ -26,16 +67,28 @@ const IssuesPage = async ({ searchParams }: Props) => {
             <Table.Root variant="surface">
                 <Table.Header>
                     <Table.Row>
-                        <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
-                        <Table.ColumnHeaderCell className="hidden md:table-cell">
-                            Status
-                        </Table.ColumnHeaderCell>
-                        <Table.ColumnHeaderCell className="hidden md:table-cell">
-                            Created
-                        </Table.ColumnHeaderCell>
-                        <Table.ColumnHeaderCell className="hidden md:table-cell">
-                            Assigned to
-                        </Table.ColumnHeaderCell>
+                        {columns.map((column) => (
+                            <Table.ColumnHeaderCell
+                                key={column.value}
+                                className={column.className}
+                            >
+                                <NextLink
+                                    href={`/issues?${buildQuery(
+                                        column.value,
+                                        orderDirection === "asc" ? "desc" : "asc"
+                                    )}`}
+                                >
+                                    {column.label}
+                                </NextLink>
+                                {column.value === orderBy && orderDirection === "asc" && (
+                                    <RxArrowUp className="inline" />
+                                )}
+                                {column.value === orderBy &&
+                                    orderDirection === "desc" && (
+                                        <RxArrowDown className="inline" />
+                                    )}
+                            </Table.ColumnHeaderCell>
+                        ))}
                     </Table.Row>
                 </Table.Header>
                 <Table.Body>
